f6a5b17ad1f3919492c661672690bda68206fa8f
Kekhayev Rodion <Kekhayev.r@mail.ru>
2025-11-23 13:31:05 +0500

    deploy v1

--- BirQadamDjango/volunteer_project/settings.py
+++ BirQadamDjango/volunteer_project/settings.py
@@ -46,6 +46,8 @@ CORS_ALLOW_ALL_ORIGINS = DEBUG  # True только в режиме разраб
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:8002",
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
@@ -58,6 +60,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://172.31.0.186:8000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://birqadam.almau.edu.kz",
+    "http://192.168.45.232:8002"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -67,6 +71,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://birqadam.almau.edu.kz",
+    "http://192.168.45.232:5173"
 ]
 
 CORS_ALLOW_HEADERS = [
