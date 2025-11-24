1d095fdbc628de43afea7ace0c0c0a8c38699d8e
Kekhayev Rodion <Kekhayev.r@mail.ru>
2025-11-24 09:57:40 +0500

    deploy v2

--- BirQadamDjango/frontend/src/services/http.ts
+++ BirQadamDjango/frontend/src/services/http.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8000';
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? 'http://192.168.45.232:8002';
 
 export const httpClient = axios.create({
   baseURL: apiBaseUrl,
